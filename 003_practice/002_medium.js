b4597bebd7786e5c51732b1cbf225981b1dac157
TKHTS <sakuranohanabira1269@gmail.com>
2021-12-14 18:01:23 +0900

    Fix isPrime()

--- 003_practice/002_medium.js
+++ 003_practice/002_medium.js
@@ -91,6 +91,18 @@ function isPalindrome(str) {
  *
  */
 function isPrime(num) {
+  if(num === 1){
+    return false;
+  }else if(num === 2){
+    return true;
+  }else{
+    for(let i = 2; i < num; i++){
+      if(num % i === 0){
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 /**
